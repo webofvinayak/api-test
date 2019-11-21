862b0df5ed4bfd73713df9705f57e8f4ce8a0253
Vinayak Mandrekar <k.vinayak05@gmail.com>
2019-11-21 19:27:58 +0530

    api with send method completed

--- index.js
+++ index.js
@@ -11,16 +11,24 @@ res.sendfile(__dirname+"/index.html");
 });
 
 app.post("/",function(req,res){
-console.log(req.body.crypto);
 
- request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",function(error,response,body){
+//console.log(req.body.crypto);
+
+	var crypto=req.body.crypto;
+	var fiat=  req.body.fiat;
+
+	var baseURL="https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+	var finalURL=baseURL+crypto+fiat;
+
+
+ request(finalURL,function(error,response,body){
  //console.log(body);
    
 	 var data=JSON.parse(body);
 	 var price= data.last;
 	 console.log(price);
 
-	 res.send("<h1> 'Current price of bitcoin in USD is"+price+"</h1>");
+	 res.send("<h1> Current price of "+crypto+" in "+fiat+ " is "+price+"</h1>");
  
  });	
 
